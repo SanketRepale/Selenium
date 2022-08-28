e360e1654523650bb5243bf3104736d98881694e
sanke <sanke@LAPTOP-E8REBT07>
2022-08-28 21:59:45 +0530

    Add project files.

---
+++ Selenium/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using OpenQA.Selenium.Firefox;
+
+namespace selenium
+{
+    class program
+    {
+        IWebDriver driver = new ChromeDriver();
+        static void Main(string[] args)
+        {
+        }
+        [SetUp]        
+        
+        public void Initialize()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com/");
+        }
+        [Test]
+        public void ExecuteTest()
+        {
+            IWebElement element = driver.FindElement(By.Name("q"));
+            element.SendKeys("executeautomation");
+
+        }
+        [TearDown]
+        public void cleanup()
+        {
+            driver.Close();
+        }
+    }
+}
+
